0e531d2dd7e5e8dfa3a5b610289518836e51737e
Tekno125 <tekno125@outlook.com>
2025-03-28 15:22:25 +0700

    By commenting out L = scipy.sparse.csr_matrix...--> closed_form_matting.py

--- closed_form_matting/closed_form_matting.py
+++ closed_form_matting/closed_form_matting.py
@@ -105,7 +105,7 @@ def compute_laplacian(img: np.ndarray, mask=None, eps: float =10**(-7), win_rad:
     L = scipy.sparse.coo_matrix((nz_indsVal, (nz_indsRow, nz_indsCol)), shape=(h*w, h*w))
 
     # rewrite L in CSR format
-    L = scipy.sparse.csr_matrix((nz_indsVal, nz_indsCol, np.arange(0, nz_indsVal.shape[0] + 1, win_size)), shape=(h*w, h*w))
+    # L = scipy.sparse.csr_matrix((nz_indsVal, nz_indsCol, np.arange(0, nz_indsVal.shape[0] + 1, win_size)), shape=(h*w, h*w))
     return L
 
 
